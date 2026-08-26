3509b5e7b6224215bdf091e15077eabdb266d202
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-26 18:27:13 -0300

    add evidencias e AppNavigation e TarefaViewModel integration MainActivity

--- app/src/main/java/eduardorisch/github/com/to_do_list_trabalho/MainActivity.kt
+++ app/src/main/java/eduardorisch/github/com/to_do_list_trabalho/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import eduardorisch.github.com.to_do_list_trabalho.navigation.AppNavigation
+
 import eduardorisch.github.com.to_do_list_trabalho.ui.theme.TodolistTrabalhoTheme
+import eduardorisch.github.com.to_do_list_trabalho.viewmodel.TarefaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodolistTrabalhoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodolistTrabalhoTheme {
-        Greeting("Android")
     }
 }
